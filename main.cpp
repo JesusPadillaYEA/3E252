e8381e8d74ead37b700b4cb5a80b35bb9b72a3d3
JesusPadillaSRank <a24310401@live.ceti.mx>
2025-09-05 03:19:00 +0000

    mi primer commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string nombre;
+    cout<<"escribe tu nombre"<<endl;
+    cin>>nombre;
+    cout<<"Hola "<<nombre<<endl;
+    return 0;
+}
