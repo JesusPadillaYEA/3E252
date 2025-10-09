0ff1bd1f5e78db1bc6e49df4e3fa152a4535144a
JesusPadillaSRank <a24310401@live.ceti.mx>
2025-10-09 01:23:06 +0000

    practica 2.1

--- src/Punteros.cpp
+++ src/Punteros.cpp
@@ -10,12 +10,19 @@ int main (){
     *pNum = 25;
 
     cout << "direccion: " << pNum << '\n';
-    
-    cout << "valor: " << *pNum << '\n';
-    
-    cout << "valor: " << &pNum << '\n';
+    cout << "valor: " << *pNum << '\n';    
+    cout << "primera direccion: " << &pNum << '\n';
 
-    cout << sizeof(pNum) << '\n';
+
+    cout << "Tipos de datos: " << endl;
+
+    cout << "Tamaño int: " << sizeof(int) << endl; 
+    cout << "Tamaño long: " << sizeof(long) << endl; 
+    cout << "Tamaño float: " << sizeof(float) << endl; 
+    cout << "Tamaño double: " << sizeof(double) << endl; 
+    cout << "Tamaño char: " << sizeof(char) << endl; 
+    cout << "Tamaño bool: " << sizeof(bool) << endl; 
+    cout << "Tamaño byte: " << sizeof(byte) << endl; 
 
     return 0;
 
